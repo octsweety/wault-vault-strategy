fbaa4c67d92a7c1b85586c4c90e05bbd5124e763
sagheerali1008 <alisahheer1008@gmail.com>
2021-03-27 12:44:36 +0400

    Fixed caculatiion issue when deposit

--- contracts/vaults/WaultBusdVault.sol
+++ contracts/vaults/WaultBusdVault.sol
@@ -76,17 +76,15 @@ contract WaultBusdVault is ERC20, IVault {
     function deposit(uint256 _amount) public override {
         require(_amount >= min.mul(1e18), "deposit amount is too small");
 
-        uint256 _pool = balance();
+        // uint256 _pool = balance();
         uint256 _before = token.balanceOf(address(this));
         token.safeTransferFrom(msg.sender, address(this), _amount);
         uint256 _after = token.balanceOf(address(this));
         _amount = _after.sub(_before); // Additional check for deflationary tokens
-        uint256 shares = 0;
-        if (totalSupply() == 0) {
-            shares = _amount;
-        } else {
-            shares = (_amount.mul(totalSupply())).div(_pool);
-        }
+        uint256 shares = _amount;
+        // if (totalSupply() > 0) {
+        //     shares = (_amount.mul(totalSupply())).div(_pool);
+        // }
         _mint(msg.sender, shares);
         invest();
     }
